b2711c1f9df86eb7a3cf59c8cba38c8cb081d803
palak jaiswal <jaiswalpalak301205@gmail.com>
2026-05-22 15:55:24 +0530

    all sructure has been deleted

--- .next/dev/static/chunks/app_page_0kcl1js.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_0a.5l5u._.js","static/chunks/node_modules_three_build_three_module_12n2-ho.js","static/chunks/0r.7_react-reconciler_0ji56..._.js","static/chunks/node_modules_@react-three_fiber_dist_0nw~o9.._.js","static/chunks/node_modules_framer-motion_dist_es_03891he._.js","static/chunks/node_modules_02o5ipz._.js","static/chunks/_0~fi6x~._.js"],
-    source: "dynamic"
-});
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jgTQyCux9nhMQZnDrWLlrlOysn88i2xExTGHd6pgDR0=\"\n}"
